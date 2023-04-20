d249f5318a5543f1b4aba83d4c25730d2f354885
Chloe ONeil <chloesporri@yahoo.com>
2023-04-20 11:23:44 -0700

    add required to name, state and national in Park model

--- ParkApi/Models/Park.cs
+++ ParkApi/Models/Park.cs
@@ -3,8 +3,12 @@ namespace ParkApi.Models
   public class Park
   {
     public int ParkId { get; set; }
+    [Required]
+    [StringLength(25)]
     public string Name { get; set; }
+    [Required]
     public string State { get; set; }
+    [Required]
     public bool National { get; set; }
   }
 }
