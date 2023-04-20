b3756607bba40c0e4b0ae7a44430911fa07999ce
Chloe ONeil <chloesporri@yahoo.com>
2023-04-20 10:33:36 -0700

    migrations done for seed data

---
+++ ParkApi/Migrations/20230420172916_SeedData.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParkApi.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Park",
+                columns: new[] { "ParkId", "Name", "National", "State" },
+                values: new object[,]
+                {
+                    { 1, "Crater Lake", true, "Oregon" },
+                    { 2, "Ainsworth", false, "Oregon" },
+                    { 3, "Bates", false, "Oregon" },
+                    { 4, "Angel Island", false, "California" },
+                    { 5, "Van Damme", false, "California" },
+                    { 6, "Yosemite", true, "California" },
+                    { 7, "Joshua Tree", true, "California" },
+                    { 8, "Big Bend", true, "Texas" },
+                    { 9, "Big Spring", false, "Texas" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Park",
+                keyColumn: "ParkId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Park",
+                keyColumn: "ParkId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Park",
+                keyColumn: "ParkId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Park",
+                keyColumn: "ParkId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Park",
+                keyColumn: "ParkId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Park",
+                keyColumn: "ParkId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Park",
+                keyColumn: "ParkId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Park",
+                keyColumn: "ParkId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Park",
+                keyColumn: "ParkId",
+                keyValue: 9);
+        }
+    }
+}
--- ParkApi/Models/ParkApiContext.cs
+++ ParkApi/Models/ParkApiContext.cs
@@ -15,15 +15,15 @@ namespace ParkApi.Models
     {
       builder.Entity<Park>()
               .HasData(
-                new Park { ParkId = 1, Name = "Crater Lake", State = "Oregon", National = True},
-                new Park { ParkId = 2, Name = "Ainsworth", State = "Oregon", National = False},
-                new Park { ParkId = 3, Name = "Bates", State = "Oregon", National = False},
-                new Park { ParkId = 4, Name = "Angel Island", State = "California", National = Flase},
-                new Park { ParkId = 5, Name = "Van Damme", State = "California", National = False},
-                new Park { ParkId = 6, Name = "Yosemite", State = "California", National = True},
-                new Park { ParkId = 7, Name = "Joshua Tree", State = "California", National = True},
-                new Park { ParkId = 8, Name = "Big Bend", State = "Texas", National = True},
-                new Park { ParkId = 9, Name = "Big Spring", State = "Texas", National = False}
+                new Park { ParkId = 1, Name = "Crater Lake", State = "Oregon", National = true},
+                new Park { ParkId = 2, Name = "Ainsworth", State = "Oregon", National = false},
+                new Park { ParkId = 3, Name = "Bates", State = "Oregon", National = false},
+                new Park { ParkId = 4, Name = "Angel Island", State = "California", National = false},
+                new Park { ParkId = 5, Name = "Van Damme", State = "California", National = false},
+                new Park { ParkId = 6, Name = "Yosemite", State = "California", National = true},
+                new Park { ParkId = 7, Name = "Joshua Tree", State = "California", National = true},
+                new Park { ParkId = 8, Name = "Big Bend", State = "Texas", National = true},
+                new Park { ParkId = 9, Name = "Big Spring", State = "Texas", National = false}
               );
     }
   }
