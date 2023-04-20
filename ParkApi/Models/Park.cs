ef16c9657cb5a2381a85a0b287755596133e166b
Chloe ONeil <chloesporri@yahoo.com>
2023-04-20 10:09:12 -0700

    add Park model and boiler

---
+++ ParkApi/Models/Park.cs
@@ -0,0 +1,10 @@
+namespace ParkApi.Models
+{
+  public class Park
+  {
+    public int ParkId { get; set; }
+    public string Name { get; set; }
+    public string State { get; set; }
+    public bool National { get; set; }
+  }
+}
