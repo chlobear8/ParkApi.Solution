6a78727e07e22c45262fe9c74b18aa92667d5783
Chloe ONeil <chloesporri@yahoo.com>
2023-04-20 11:01:25 -0700

    add post route

--- ParkApi/Controllers/ParkController.cs
+++ ParkApi/Controllers/ParkController.cs
@@ -41,5 +41,39 @@ namespace ParkApi.Controllers
       await _db.SaveChangesAsync();
       return CreatedAtAction(nameof(GetPark), new { id = park.ParkId }, park);
     }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Park park)
+    {
+      if (id != park.ParkId)
+      {
+        return BadRequest();
+      }
+
+      _db.Park.Update(park);
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!ParkExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    private bool ParkExists(int id)
+    {
+      return _db.Park.Any(e => e.ParkId == id);
+    }
   }
 }
