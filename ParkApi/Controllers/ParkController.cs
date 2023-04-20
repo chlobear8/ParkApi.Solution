54ecd27324a03f134b2b8f80df15d0d88a3b2959
Chloe ONeil <chloesporri@yahoo.com>
2023-04-20 10:37:17 -0700

    add get route for parkApi

---
+++ ParkApi/Controllers/ParkController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParkApi.Models;
+
+namespace ParkApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ParkController : ControllerBase
+  {
+    private readonly ParkController(ParkApiContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Park>>> Get()
+    {
+      return await _db.Park.ToListAsync();
+    }
+  }
+}
